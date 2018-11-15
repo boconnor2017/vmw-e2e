d0822738b30ce71b58b3f4584a803159952acaba
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2018-11-15 14:57:39 -0500

    Update create-api-session.sh

--- api/HCX/create-api-session.sh
+++ api/HCX/create-api-session.sh
@@ -1,5 +1,7 @@
 # Created by VMWare Professional Services
 # Author: Brendan O'Connor
 #
-# Edit the <username> <password> and <hcx_manager_fqdn> parameters below
+# INSTRUCTIONS:
+#  1. Edit the <username@vsphere.local> <password> and <hcx_manager_fqdn> parameters below
+#  2. The response will provide a parameter called x-hm-authorization. This is the Authorization token for subsequent API calls.
 curl -i  -H "Accept: application/json" -H "Content-Type: application/json"  --request POST --data '{"username": "<username@vsphere.local>", "password": "<password>"}'  https://<hcx_manager_fqdn>/hybridity/api/sessions --insecure
