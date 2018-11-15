dd0567a95705fe1a1dd85fe581af3a262c672bfd
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2018-11-15 14:53:50 -0500

    Update create-api-session.sh

--- api/HCX/create-api-session.sh
+++ api/HCX/create-api-session.sh
@@ -2,4 +2,4 @@
 # Author: Brendan O'Connor
 #
 # Edit the <username> <password> and <hcx_manager_fqdn> parameters below
-curl -i  -H "Accept: application/json" -H "Content-Type: application/json"  --request POST --data '{"username": "<username>", "password": "<password>"}'  https://<hcx_manager_fqdn>/hybridity/api/sessions --insecure
+curl -i  -H "Accept: application/json" -H "Content-Type: application/json"  --request POST --data '{"username": "<username@vsphere.local>", "password": "<password>"}'  https://<hcx_manager_fqdn>/hybridity/api/sessions --insecure
