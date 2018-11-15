60184be9504d71219eabab073a6b8107034eebfc
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2018-11-15 14:41:22 -0500

    Update create-api-session.sh

--- api/HCX/create-api-session.sh
+++ api/HCX/create-api-session.sh
@@ -1,2 +1,5 @@
+# Created by VMWare Professional Services
+# Author: Brendan O'Connor
 #
+# Edit the <username> <password> and <hcx_manager_fqdn> parameters below
 curl -i  -H "Accept: application/json" -H "Content-Type: application/json"  --request POST --data '{"username": "<username>", "password": "<password>"}'  https://<hcx_manager_fqdn>/hybridity/api/sessions --insecure
