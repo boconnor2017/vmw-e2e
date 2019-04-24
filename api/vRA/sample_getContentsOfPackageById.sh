4fc9d67e2c1fda258b3c9dab5f18a0dcb80a1192
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2019-04-24 15:04:41 -0400

    Update sample_getContentsOfPackageById.sh

--- api/vRA/sample_getContentsOfPackageById.sh
+++ api/vRA/sample_getContentsOfPackageById.sh
@@ -1,3 +1,3 @@
-curl --insecure -X GET --header 'Accept: text/html' -H "Authorization: Bearer $token" 'https://<vra-fqdn>/content-management-service/api/packages/b23c63fd-623c-496b-8f83-277b0c7026d1/contents' >> package-content.json
+curl --insecure -X GET --header 'Accept: text/html' -H "Authorization: Bearer $token" 'https://<vra-fqdn>/content-management-service/api/packages/<package id>/contents' >> package-content.json
 cat package-content.json | jq '.content[]'
 rm package-content.json
