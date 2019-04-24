d7abf0d7c8c12bf1e6377756db0b26b5096dd0a8
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2019-04-24 14:24:15 -0400

    Update sample_exportPackage.sh

--- api/vRA/sample_exportPackage.sh
+++ api/vRA/sample_exportPackage.sh
@@ -1,1 +1,1 @@
-curl --insecure -s -H "Content-Type: application/zip" -H "Authorization: Bearer $token" https://<vra-fqdn>/content-management-service/api/packages/24b5ffa3-f718-4072-b437-dc9efed9a44a -o E2ETest1.zip
+curl --insecure -s -H "Accept: application/zip" -H "Authorization: Bearer $token" https://<vra-fqdn>/content-management-service/api/packages/<package id> -o E2ETest1.zip
