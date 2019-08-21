817f227389f9fe54de554a3f981159bc77a7e89a
Brendan O'Connor <31047966+boconnor2017@users.noreply.github.com>
2019-08-21 10:44:42 -0400

    Update e2e-vcf-dns-entries.ps1

--- solutions/VCF-Prereqs/e2e-vcf-dns-entries.ps1
+++ solutions/VCF-Prereqs/e2e-vcf-dns-entries.ps1
@@ -1,38 +1,52 @@
 <#
-
 Version: 1.0
 Author: Brendan O'Connor (VMWare Professional Services)
 Date: August 2019
+
 Disclaimer: this solution is not a validated or copywrite solution from VMWare.
             This solution is an open source tool for E2E participants to
             utilize at their discression. You may copy, edit, and redistribute
             this solution as you like.
-How to use: run this script against a DNS server to automatically create necessary
-            DNS entries for VMWare Cloud Foundation
 
-MANAGEMENT WORKLOAD DOMAIN SAMPLE DNS ENTRIES
+How to use: Edit the values and run this script against a DNS server to automatically create necessary
+            DNS entries for VMWare Cloud Foundation. The values should be based on completed information
+            in the CloudBuilder spreadsheet. 
+
+
+MANAGEMENT WORKLOAD DOMAIN SAMPLE DNS ENTRIES (see VCF Documentation on my.vmware.com for details)
 sfo01m01sddcmgr	sfo01.rainpole.local	172.16.11.60	SDDC Manager
 sfo01m01psc01	sfo01.rainpole.local	172.16.11.61	Platform Services Controller 01
-sfo01m02psc02	sfo01.rainpole.local	172.16.11.63	Platform Services Controller 02
+sfo01m02psc02	sfo01.rainpole.local	172.16.11.62	Platform Services Controller 02
 sfo01m01vc01	sfo01.rainpole.local	172.16.11.63	vCenter Server
 sfo01m01esx01	sfo01.rainpole.local	172.16.11.101	ESXi host 01
 sfo01m01esx02	sfo01.rainpole.local	172.16.11.102	ESXi host 02
 sfo01m01esx03	sfo01.rainpole.local	172.16.11.103	ESXi host 03
 sfo01m01esx04	sfo01.rainpole.local	172.16.11.104	ESXi host 04
 sfo01m01nsx01	sfo01.rainpole.local	172.16.11.64	NSX-V Manager
-sfo01m01nsxc01							        172.16.11.65	NSX-V Controller 01
-sfo01m01nsxc02							        172.16.11.66	NSX-V Controller 02
-sfo01m01nsxc03							        172.16.11.67	NSX-V Controller 03
-sfo01w01vc01	sfo01.rainpole.local	172.16.11.68	Additional vCenter Server for VI Workload #1
-sfo01w01nsx01	sfo01.rainpole.local	172.16.11.69	Additional NSX-V Manager for VI Workload #1
-sfo01w02vc01	sfo01.rainpole.local	172.16.11.70	Additional vCenter Server for VI Workload #2
-sfo01w02nsx01	sfo01.rainpole.local	172.16.11.71	NSX-T Manager for Workload #2
-sfo01w02nsxc01							        172.16.11.120	NSX-T Controller 01 for Workload #2
-sfo01w02nsxc02							        172.16.11.121	NSX-T Controller 02 for Workload #2
-sfo01w02nsxc03							        172.16.11.122	NSX-T Controller 03 for Workload #2
 
+VI WORKLOAD DOMAIN #1 (repeat this section for any additional workload domains)
+sfo01w01vc01	sfo01.rainpole.local	172.16.11.68	Additional vCenter Server for VI Workload Domain #1
+sfo01w01nsx01	sfo01.rainpole.local	172.16.11.69	Additional NSX-V Manager for VI Workload Domain #1
+sfo01w01esx01	sfo01.rainpole.local	172.16.17.101	ESXi host 05
+sfo01w01esx02	sfo01.rainpole.local	172.16.17.102	ESXi host 06
+sfo01w01esx03	sfo01.rainpole.local	172.16.17.103	ESXi host 07
+sfo01w01esx04	sfo01.rainpole.local	172.16.17.104	ESXi host 08
 #>
+
+
 Add-DnsServerResourceRecordA -Name "sfo01m01sddcmgr" -ZoneName "sfo01.rainpole.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.11.60"
+Add-DnsServerResourceRecordA -Name "sfo01m01psc01" -ZoneName "sfo01.rainpole.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.11.61"
+Add-DnsServerResourceRecordA -Name "sfo01m02psc02" -ZoneName "sfo01.rainpole.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.11.62"
+Add-DnsServerResourceRecordA -Name "sfo01m01vc01" -ZoneName "sfo01.rainpole.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.11.63"
+Add-DnsServerResourceRecordA -Name "sfo01m01esx01" -ZoneName "sfo01.rainpole.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.11.101"
+Add-DnsServerResourceRecordA -Name "sfo01m01esx02" -ZoneName "sfo01.rainpole.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.11.102"
+Add-DnsServerResourceRecordA -Name "sfo01m01esx03" -ZoneName "sfo01.rainpole.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.11.103"
+Add-DnsServerResourceRecordA -Name "sfo01m01esx04" -ZoneName "sfo01.rainpole.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.11.104"
+Add-DnsServerResourceRecordA -Name "sfo01m01nsx01" -ZoneName "sfo01.rainpole.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.11.64"
+Add-DnsServerResourceRecordA -Name "sfo01w01vc01" -ZoneName "sfo01.rainpole.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.11.68"
+Add-DnsServerResourceRecordA -Name "sfo01w01nsx01" -ZoneName "sfo01.rainpole.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.11.69"
 
-
-# TO BE CONTINUED... WORK IN PROGRESS
+Add-DnsServerResourceRecordA -Name "sfo01w01esx01" -ZoneName "sfo01.rainpole.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.11.101"
+Add-DnsServerResourceRecordA -Name "sfo01w01esx01" -ZoneName "sfo01.rainpole.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.11.102"
+Add-DnsServerResourceRecordA -Name "sfo01w01esx01" -ZoneName "sfo01.rainpole.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.11.103"
+Add-DnsServerResourceRecordA -Name "sfo01w01esx01" -ZoneName "sfo01.rainpole.local" -AllowUpdateAny -CreatePtr -IPv4Address "172.16.11.104"
